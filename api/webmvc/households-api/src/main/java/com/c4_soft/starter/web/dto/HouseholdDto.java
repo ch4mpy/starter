c49368448104c45b1f2ad8790df135edd964fddf
Jérôme Wacongne <ch4mp@c4-soft.com>
2021-09-13 16:43:44 -1000

    Force collection relation name

--- api/webmvc/households-api/src/main/java/com/c4_soft/starter/web/dto/HouseholdDto.java
+++ api/webmvc/households-api/src/main/java/com/c4_soft/starter/web/dto/HouseholdDto.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.springframework.hateoas.server.core.Relation;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @XmlRootElement
+@Relation(collectionRelation = "households")
 public class HouseholdDto implements Serializable {
 	private static final long serialVersionUID = -5043590484600129827L;
 
