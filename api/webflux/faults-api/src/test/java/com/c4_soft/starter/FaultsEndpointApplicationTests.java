81a685240be61d1f2b3256d1ccf4c27acd4c7671
ch4mpy <ch4mp@c4-soft.com>
2021-10-10 08:20:36 -1000

    Run unit tests without authorisation-server

--- api/webflux/faults-api/src/test/java/com/c4_soft/starter/FaultsEndpointApplicationTests.java
+++ api/webflux/faults-api/src/test/java/com/c4_soft/starter/FaultsEndpointApplicationTests.java
@@ -2,12 +2,17 @@ package com.c4_soft.starter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class FaultsEndpointApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@MockBean
+	JwtDecoder jwtDecoder;
+
+	@Test
+	void contextLoads() {
+	}
 
 }
--- api/webmvc/households-api/src/test/java/com/c4_soft/starter/HouseholdsEndpointApplicationTests.java
+++ api/webmvc/households-api/src/test/java/com/c4_soft/starter/HouseholdsEndpointApplicationTests.java
@@ -2,9 +2,14 @@ package com.c4_soft.starter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class HouseholdsEndpointApplicationTests {
+
+	@MockBean
+	JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
